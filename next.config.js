c5960a4fdc01db2b9c0505a91f9741f5920b0181
Wilfried DJOPA <37931403+wdjopa@users.noreply.github.com>
2023-01-11 10:38:18 +0100

    setup lambda deployment

--- next.config.js
+++ next.config.js
@@ -2,9 +2,11 @@
 const nextConfig = {
   compress: true,
   reactStrictMode: false,
+  swcMinify: true,
   images: {
     domains: ["dashboard.genuka.com", "cdn.genuka.com"],
   },
+  output: "standalone", // THIS IS IMPORTANT
 };
 
 module.exports = nextConfig;
