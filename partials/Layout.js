a7dd58a0e93ae3f5fd161c96912aa2d910a06151
Wilfried DJOPA <37931403+wdjopa@users.noreply.github.com>
2023-12-01 20:44:14 +0100

    update

--- partials/Layout.js
+++ partials/Layout.js
@@ -7,10 +7,16 @@ import Footer from "./Footer.js";
 import Header from "./Header.js";
 
 function Layout({ company, children, head }) {
-  
+  const model = company.settings.default_template?.product_layout || "square";
+
   const { loading, error } = useGenukaState();
   return (
-    <div className="container md:px-6  px-4 mx-auto">
+    <div
+      className={
+        (model === "line" ? "" : "max-w-4xl ") +
+        " container md:px-6 px-4 mx-auto"
+      }
+    >
       {head && <Head>{head}</Head>}
       <Header company={company} />
       <main className="mt-4 mb-10 ">{children}</main>
